675a22fe9484902516537d7d539ee0d5665caa65
harishtekgenie <challaharish.vita@gmail.com>
2026-03-24 18:12:26 +0530

    fix: Update CORS configuration to include additional frontend origin for credentials compatibility

--- src/app.js
+++ src/app.js
@@ -20,7 +20,8 @@ app.use(cors({
         'http://localhost:3000',
         'http://localhost:3001',
         'http://127.0.0.1:3000',
-        'http://127.0.0.1:3001'
+        'http://127.0.0.1:3001',
+        'https://cloths-ecommerce-frontend.onrender.com'
     ],
     credentials: true
 }));
